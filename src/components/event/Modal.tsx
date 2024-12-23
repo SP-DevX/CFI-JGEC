2c8a37bf72f240e364425daf77fbfd07b3dd3279
Souhardya03 <souhardyadeb2016@gmail.com>
2024-12-23 11:55:53 +0530

    Updated Modal

--- src/components/event/Modal.tsx
+++ src/components/event/Modal.tsx
@@ -54,33 +54,39 @@ const Modal: React.FC<props> = ({ eventId }) => {
 			<div
 				className="modal"
 				role="dialog">
-				<div className="lg:w-1/4 modal-box p-0 overflow-hidden md:h-[30vh] h-[80vh] lg:h-[80vh] bg-white">
-					<div className="modal-back flex items-center flex-col lg:gap-8 gap-6 justify-center curve h-[95%] w-full">
+				<div className="xl:w-[60%]  2xl:h-[80vh] modal-box p-0 overflow-hidden  md:h-[75vh]  xs:h-[70vh] xxs:h-[70vh] h-[70vh] lg:h-[70vh] xl:h-[70vh] bg-white">
+					<div className="modal-back py-4 flex items-center flex-col lg:gap-8 gap-6 justify-evenly curve h-[95%] w-full">
 						<div className="text-xl">
 							<div className="flex modal_text">
-								<div className="bg-white left-0  h-12 w-12 flex items-center rounded-lg justify-center z-50 -rotate-6">
+								<div className="bg-white left-0 h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center z-50 -rotate-6">
 									<div>R</div>
 								</div>
-								<div className="bg-green-300 h-12 w-12 flex items-center rounded-lg justify-center left-8 z-40 rotate-6">
-									A
+								<div className="bg-green-300  h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-8 z-40 rotate-6">
+									E
 								</div>
-								<div className="bg-white h-12 w-12 flex items-center rounded-lg justify-center left-16 z-30 -rotate-6">
-									N
+								<div className="bg-white  h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-16 z-30 -rotate-6">
+									S
 								</div>
-								<div className="bg-red-500 h-12 w-12 flex items-center rounded-lg justify-center left-24 z-20 rotate-6">
-									K
+								<div className="bg-red-500 h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-24 z-20 rotate-6">
+									U
 								</div>
-								<div className="bg-white h-12 w-12 flex items-center rounded-lg justify-center left-32 -rotate-6">
+								<div className="bg-white h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-32 -rotate-6">
+									L
+								</div>
+								<div className="bg-blue-400 h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-32 rotate-6">
+									T
+								</div>
+								<div className="bg-white h-10 w-10 md:h-12 md:w-12 lg:h-12 lg:w-12 flex items-center rounded-lg justify-center left-32 -rotate-6">
 									S
 								</div>
 							</div>
 						</div>
-						<div className="text-2xl font-bold">Leaderboard</div>
+						
 						{/* Winners */}
 						<div className="flex justify-evenly w-full">
 							{top3Teams[1] && (
-								<div className="flex mt-2 w-1/2 items-center gap-4 flex-col">
-									<div className="w-14 h-14 mt-3 -rotate-6 border-[4px] rounded-xl border-[#979595]">
+								<div className="flex mt-2 w-1/2 items-center gap-3 flex-col">
+									<div className="w-14 h-14 md:h-20 md:w-20 mt-3 -rotate-6 border-[4px] rounded-xl border-[#979595]">
 										<img
 											src={top3Teams[1]?.teamLogo}
 											className="img rounded-lg"
@@ -90,18 +96,18 @@ const Modal: React.FC<props> = ({ eventId }) => {
 											<Image
 												src={silver_badge}
 												alt="First_Badge"
-												className="absolute w-10 top-[2em]"
+												className="absolute w-10 md:top-[3em] top-[2em]"
 											/>
 										</div>
 									</div>
-									<div className="lg:text-lg  line-clamp-2 text-center">
+									<div className="lg:text-[15px] text-[15px] md:text-[18px] line-clamp-2 pl-2 text-center">
 										{top3Teams[1]?.teamName}
 									</div>
 								</div>
 							)}
 							{top3Teams[0] && (
 								<div className="flex w-1/2 items-center gap-4 flex-col">
-									<div className="w-[5rem] h-[5rem] mt-3 rotate-0 border-[4px] rounded-xl border-[#ede103]">
+									<div className="md:h-24 md:w-24 w-20 h-20 mt-3 rotate-0 border-[4px] rounded-xl border-[#ede103]">
 										<img
 											src={top3Teams[0]?.teamLogo}
 											className="img rounded-lg"
@@ -115,14 +121,14 @@ const Modal: React.FC<props> = ({ eventId }) => {
 											/>
 										</div>
 									</div>
-									<div className="text-lg line-clamp-2 text-center">
+									<div className="md:text-xl text-lg line-clamp-2 text-center">
 										{top3Teams[0]?.teamName}
 									</div>
 								</div>
 							)}
 							{top3Teams[2] && (
-								<div className="flex mt-2 w-1/2 items-center gap-4 flex-col">
-									<div className="w-14 h-14 mt-3 rotate-6 border-[4px] rounded-xl border-[#9c6a05]">
+								<div className="flex mt-2 w-1/2 items-center gap-5 flex-col">
+									<div className="md:w-20 md:h-20 w-14 h-14 md:mt-3 mt-3 rotate-6 border-[4px] rounded-xl border-[#9c6a05]">
 										<img
 											src={top3Teams[2]?.teamLogo}
 											className="img rounded-lg"
@@ -132,11 +138,11 @@ const Modal: React.FC<props> = ({ eventId }) => {
 											<Image
 												src={bronze_badge}
 												alt="First_Badge"
-												className="absolute w-12 top-[2em]"
+												className="absolute w-12 md:top-[3em] top-[2em]"
 											/>
 										</div>
 									</div>
-									<div className=" lg:text-lg text-center line-clamp-2 ">
+									<div className=" lg:text-[15px] md:text-[18px] text-[14px] text-center line-clamp-2 ">
 										{top3Teams[2]?.teamName}
 									</div>
 								</div>
@@ -144,7 +150,7 @@ const Modal: React.FC<props> = ({ eventId }) => {
 						</div>
 
 						{/* Other rankers */}
-						<div className="flex flex-col w-full overflow-y-auto h-[35%] no-scrollbar px-8 pb-6 gap-2">
+						<div className="flex flex-col w-full overflow-y-auto h-[35%] no-scrollbar lg:px-8 px-4 pb-6 gap-2">
 							{otherTeams &&
 								otherTeams.map((data, index) => (
 									<div
@@ -163,9 +169,7 @@ const Modal: React.FC<props> = ({ eventId }) => {
 											</div>
 											<div className="font-medium">{data.teamName}</div>
 										</div>
-										<div className="pr-3 font-medium">
-											{data.members.length}
-										</div>
+										
 									</div>
 								))}
 						</div>
